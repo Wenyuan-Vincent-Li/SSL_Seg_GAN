65fa9b8b5d007472d9934867cdf2d9d41ff87a61
wenyuan <liwenyuan.zju@gmail.com>
2020-01-08 15:42:11 -0800

    continue to train

--- train_continue.py
+++ train_continue.py
@@ -5,8 +5,14 @@ from Training import functions
 opt = TrainOptions().parse()
 opt.alpha = 0.1
 opt.name = 'prostateHD'
+
+# opt.name = 'colon'
+# opt.dataroot = './Datasets/ColonPair_Fine/'
+# opt.label_nc = 6
+
+
 opt.scale_factor = 0.10
-opt.niter = 2
+opt.niter = 200
 opt.noise_amp = 1
 Gs, Ss, reals, NoiseAmp, NoiseAmpS = functions.load_trained_pyramid(opt)
 opt.reals = [[32, 32], [64, 64], [128, 128], [256, 256], [512, 512]]
