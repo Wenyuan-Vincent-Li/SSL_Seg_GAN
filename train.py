e1a02a6028bf406ce686561bb6b3c1ecb84578c7
wenyuan <liwenyuan.zju@gmail.com>
2020-02-24 15:27:40 -0800

    continue to train

--- train.py
+++ train.py
@@ -2,10 +2,11 @@ from options.train_options import TrainOptions
 from Training.train import train
 
 opt = TrainOptions().parse()
-# opt.name = 'prostateHD'
-opt.name = 'colon_Fine_100'
-opt.dataroot = './Datasets/ColonPair_Fine/'
-opt.label_nc = 6
+opt.name = 'prostateHD'  ## change #1
+# opt.name = 'colon_Fine_100'
+# opt.dataroot = './Datasets/ColonPair_Fine/'
+opt.dataroot = './Datasets/Prostate_Pair/' ## change #2
+opt.label_nc = 4
 opt.contour = False
 Gs = []
 Ss = []
@@ -13,7 +14,7 @@ NoiseAmp = []
 NoiseAmpS = []
 
 # opt.reals = [[64, 64], [128, 128], [192, 192], [256, 256], [320, 320], [384, 384], [448, 448], [512, 512]]
-opt.reals = [[64,64],[128,128],[256,256],[512,512]]
+opt.reals = [[64,64],[128,128],[192,192], [256,256]]
 opt.erod = [1, 3, 6, 13]
 opt.alpha = 0.1
 opt.scale_factor = 1.10
