ed94e97c1e3cf1bed02b15b8de95857547bcc6b3
wenyuan <liwenyuan.zju@gmail.com>
2020-02-25 13:44:09 -0800

    continue to train

--- train_continue.py
+++ train_continue.py
@@ -4,22 +4,21 @@ from Training import functions
 
 opt = TrainOptions().parse()
 opt.alpha = 0.1
-# opt.name = 'prostateHD'
+opt.name = 'prostateHD'
+# opt.name = 'colon_BI_80'
+opt.dataroot = './Datasets/ProstatePair/'
+opt.label_nc = 4
+opt.contour = False
 
-opt.name = 'colon_BI_80'
-opt.dataroot = './Datasets/ColonPair_BI/'
-opt.label_nc = 2
-opt.contour = True
 
-
-opt.scale_factor = 0.30
+opt.scale_factor = 1.20
 opt.noise_amp = 1
 Gs, Ss, reals, NoiseAmp, NoiseAmpS = functions.load_trained_pyramid(opt)
-Gs = Gs[:]
-Ss = Ss[:]
+Gs = Gs[:2]
+Ss = Ss[:2]
 opt.erod = [1, 3, 6, 13]
-opt.reals = [[64, 64], [128, 128], [256, 256], [512, 512]]
+opt.reals = [[64, 64], [128, 128], [256, 256]]
 reals = opt.reals
-opt.phase = "train_80"
+opt.phase = "train"
 opt.stop_scale = len(reals)
 train(opt, Gs, Ss, NoiseAmp, NoiseAmpS, reals)
