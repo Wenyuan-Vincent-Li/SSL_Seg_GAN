2f6dceea2ea7d1ed550ec9239f3127d13dfbf942
wenyuan <liwenyuan.zju@gmail.com>
2020-02-26 14:40:45 -0800

    continue to train

--- train.py
+++ train.py
@@ -14,10 +14,10 @@ NoiseAmp = []
 NoiseAmpS = []
 
 # opt.reals = [[64, 64], [128, 128], [192, 192], [256, 256], [320, 320], [384, 384], [448, 448], [512, 512]]
-opt.reals = [[64,64],[128,128],[256,256]]
+opt.reals = [[64,64],[128,128],[192,2192]]
 opt.erod = [1, 3, 6, 13]
 opt.alpha = 0.1
-opt.scale_factor = 1.20
+opt.scale_factor = 1.30
 opt.noise_amp = 1
 opt.stop_scale = len(opt.reals)
 opt.phase = "train"
